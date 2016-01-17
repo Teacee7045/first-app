739ef3162e5b2f89b115c7d836b67cd963428197
ChowThomas <Kirthmas@kirthmas-mbp.home>
2016-01-17 19:28:35 +0100

    added files

---
+++ DeleteMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeleteMe
+//
+//  Created by ChowThomas on 17/01/2016.
+//  Copyright © 2016 Paws. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Making a change
+
+
+}
+
